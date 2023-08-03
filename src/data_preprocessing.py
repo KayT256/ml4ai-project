d9cc63c6653511d912287d92fe027b2b76ed32f5
KayT256 <trattrieukhang0205@gmail.com>
2023-08-03 23:21:08 +0700

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,10 +4,11 @@ from transformers import AutoTokenizer
 
 def load_data(file_path):
     data = pd.read_csv(file_path, sep='\t', header=None, names=['text', 'label'])
-    return data
+    data.columns = data.iloc[0]
+    return data[1:]
 
 def preprocess_data(data):
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    tokenized_data = tokenizer(data['text'].tolist(), padding=True, truncation=True, max_length=128, return_tensors='tf')
-    labels = tf.keras.utils.to_categorical(data['label'], num_classes=2)
+    tokenized_data = tokenizer(data['sentence'].tolist(), padding=True, truncation=True, max_length=128, return_tensors='tf')
+    labels = tf.keras.utils.to_categorical(data['label'], num_classes=2, dtype='float32')
     return tokenized_data, labels
