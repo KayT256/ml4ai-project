931c7d327e484aff8726a93310f117a44719d29d
KayT256 <96287500+KayT256@users.noreply.github.com>
2023-08-04 20:04:33 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from sentiment_analysis import predict_sentiment
+from sentiment_analysis import predict_sentiment, map_sentiment_score
 
 # Set up the Streamlit app
 st.title('Sentiment Analysis App')
-st.write('Enter a paragraph to get its sentiment score (1-5):')
+st.write('Enter a paragraph to get its sentiment:')
 
 # Get user input
 text_input = st.text_area('Input text here:', height=200)
@@ -11,4 +11,6 @@ text_input = st.text_area('Input text here:', height=200)
 # Perform sentiment analysis and display the result
 if st.button('Get sentiment score'):
     sentiment_score = predict_sentiment(text_input, 'data/models/sentiment_analysis_model/')
-    st.write(f'Sentiment score: {sentiment_score}')
+    score = map_sentiment_score(sentiment_score)
+    st.write(f'Sentiment: {score}')
+    
